bb3d64ea4c213eaa1bd2b28fca5cd3d1cf57b4b3
Jenny <hinje17@gmail.com>
2021-07-06 21:54:59 -0600

    Adding initial project files

---
+++ Walkie_Textie_Initializations/init.h
@@ -0,0 +1,14 @@
+/*
+ * screen.h
+ *
+ *  Created on: Jul 1, 2021
+ *      Author: Jenny
+ */
+
+#ifndef INIT_H_
+#define INIT_H_
+
+void screen_init (void);
+void keypad_init (void);
+
+#endif /* INIT_H_ */
